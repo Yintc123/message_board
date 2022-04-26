0d89a5b0681de6b201aee4294cbd888a04cb1ef9
unknown <yiqazwsx123@gmail.com>
2022-04-26 19:22:40 +0800

    add flask.

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import *
+from dotenv import load_dotenv, dotenv_values
+
+env=str('.env.'+dotenv_values('.env')["MODE"])
+load_dotenv(override=True)
+
+app=Flask(__name__)
+app.config["JSON_AS_ASCII"]=False
+app.config["TEMPLATES_AUTO_RELOAD"]=True
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+app.debug=True
+app.run(host=dotenv_values(env)["app_host"], port=4000)
