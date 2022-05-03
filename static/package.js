27e689643baeb68f0b217efdf18b4a451515b114
Yintc <yiqazwsx123@gmail.com>
2022-05-03 20:10:02 +0800

    test

--- static/package.js
+++ static/package.js
@@ -3,9 +3,12 @@ const url={
         "url_api_message":"http://127.0.0.1:4000/api/message"
     },
     "production":{
-        "url_api_message":"http://3.115.234.130:4000/api/message"
+        "url_api_message":"http://"+aws_url2+":4000/api/message"
     }
 }
+
+const aws_url="3.115.234.130";
+const aws_url2="172.31.14.238";
 
 // const env="develop";
 const env="production";
