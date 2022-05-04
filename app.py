b6e23c0053930f1b5acb6fdad19618c180bfbcbc
Yintc <yiqazwsx123@gmail.com>
2022-05-04 19:44:31 +0800

    add ssl

--- app.py
+++ app.py
@@ -18,4 +18,4 @@ def index():
 app.register_blueprint(app1, url_prefix="/api")
 
 app.debug=True
-app.run(host=dotenv_values(env)["app_host"], port=4000, ssl_context=("/etc/ssl/mb_certificate.crt", "/etc/ssl/private.key"))
+app.run(host=dotenv_values(env)["app_host"], port=4000, ssl_context=("/etc/ssl/mb_certificate.crt", "/etc/ssl/mb_private.key"))
