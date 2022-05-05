cb2ae1727eca61d4995faaca64638c5886c90b34
Yintc <yiqazwsx123@gmail.com>
2022-05-05 11:00:06 +0800

    load balancing

--- static/package.js
+++ static/package.js
@@ -1,12 +1,13 @@
 const dn="message-board.yin888.info";
 const ip="3.115.234.130:4000";
+const lb="loadtestALB-817074138.ap-northeast-1.elb.amazonaws.com";
 
 const url={
     "develop":{
         "url_api_message":"http://127.0.0.1:4000/api/message"
     },
     "production":{
-        "url_api_message":"https://"+dn+"/api/message"
+        "url_api_message":"https://"+lb+"/api/message"
     }
 }
 
